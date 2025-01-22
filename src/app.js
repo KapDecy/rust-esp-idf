510f842871e82d89761f4c4de1e0c6d55d199bd7
HungryM1NT <ilyas.ablyazov.2004@gmail.com>
2025-01-22 12:26:18 +0300

    first visual

--- src/app.js
+++ src/app.js
@@ -3,6 +3,71 @@ let lprange = document.getElementById("lp-range");
 let serverResp = document.getElementById("server-resp");
 let url = theForm.action;
 
+
+// let cur
+let setHzForm = document.getElementById("set-hz-form")
+let modeSelector = document.getElementById("mode-selector")
+let secondHz = document.getElementById("second-hz")
+let monoModeBox = document.getElementById("mono-mode")
+
+// function sendHzRequest(url)
+
+// Handler for hide/show second Hz selector
+modeSelector.addEventListener("input", async (e) => {
+    e.preventDefault()
+
+    let mode = e.target.value
+
+    switch (mode) {
+        case "mono":
+            secondHz.hidden = true
+            document.getElementById("second-hz-range").value = 500
+            document.getElementById("second-hz-input").value = 500
+            break
+        case "stereo":
+            secondHz.hidden = false
+            break
+    }
+})
+
+
+setHzForm.addEventListener("submit", async (e) => {
+    e.preventDefault()
+
+    let form = e.currentTarget
+    let url = form.action
+
+    let entries = Object()
+    entries["first"] = parseInt(form.amountRange1.value);
+    
+    if (monoModeBox.checked) {
+        entries["second"] = entries["first"]
+    } else {
+        entries["second"] = parseInt(form.amountRange2.value)
+    }
+
+    await setHzHandler(entries)
+})
+
+
+async function setHzHandler(entries) {
+    try {
+        let resp = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify(entries),
+        });
+        // console.log(entries)
+        // serverResp.innerText = await resp.text();
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+
 theForm.addEventListener("submit", async (e) => {
     e.preventDefault();
 
@@ -12,6 +77,7 @@ theForm.addEventListener("submit", async (e) => {
 
     try {
         let entries = Object.fromEntries(new FormData(form).entries());
+        console.log(entries)
         entries["lp"] = parseInt(entries["lp"]);
         let resp = await fetch(url, {
             method: "POST",
@@ -31,7 +97,7 @@ theForm.addEventListener("submit", async (e) => {
 lprange.addEventListener('input', async (e) => {
     e.preventDefault();
 
-    let v = this.value;
+    // let v = this.value;
 
     try {
         let entries = Object();
